959e70f63db0463c80b0046c9687f087e19a1992
Erkan Bostan <profesyonel59@gmail.com>
2023-05-21 01:28:15 +0300

    API File Upload and API Consume File Upload Transactions Completed.

---
+++ Frontend-Mvc.Core/Controllers/UploadController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace Frontend_Mvc.Core.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var bytes = stream.ToArray();
+
+            ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+            var httpClient = new HttpClient();
+            await httpClient.PostAsync("http://localhost:5298/api/Upload", multipartFormDataContent);
+            return RedirectToAction("Index", "Staff");
+        }
+    }
+}
---
+++ Hotel-Api.Core/Controllers/UploadController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel_Api.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm]IFormFile file)
+        {
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(),$"Images/{fileName}");
+            var stream = new FileStream(path, FileMode.Create);
+            await file.CopyToAsync(stream);
+            return Created("", file);
+        }
+    }
+}
